250d5f30bcaaf277367b5b8de7057765961ad29f
squarekzmlviy <square-kzmlviy@outlook.jp>
2020-04-23 22:25:45 +0900

    jsonで結果を返す

--- hello.go
+++ hello.go
@@ -31,6 +31,9 @@ type NewUser struct {
     ID   int `json:"id"`
     Name string `json:"name"`
 }
+type Token struct {
+    Token string `json:"token"`
+}
 
 func main() {
 
@@ -121,6 +124,16 @@ func main() {
                 if err_JWT == nil {
                     fmt.Fprint(w, "json hello!\n")
                     fmt.Printf("POST hello! %s \n", tokenString)
+
+                    var token Token
+                    token.Token = tokenString
+                    res_json, _ := json.Marshal(token)
+                    w.Header().Set("Content-Type", "application/json")
+                    w.Write(res_json)
+
+
+
+
                 } else {
                     fmt.Fprint(w, "Not Json hello!\n")
                 }
@@ -138,7 +151,104 @@ func main() {
     http.HandleFunc("/user/get", func(w http.ResponseWriter, r *http.Request) {
         // request bodyの読み取り
     
-             //test
+            //test
+        fmt.Fprintf(w, "<h1>Test</h1>")
+
+        switch r.Method {
+
+
+            case http.MethodGet:
+                // w.WriteHeader(http.StatusCreated)
+                fmt.Fprint(w, "POST hello!\n")
+
+                header_x_token := r.Header.Get("x-token")
+                // //headerが読み込まれている確認
+                // fmt.Println(header_x_token)
+
+
+                //tokenの解釈
+                token, err := jwt.Parse(header_x_token, func(token *jwt.Token) (interface{}, error) {
+                    return []byte("himitu"), nil // CreateTokenにて指定した文字列を使います
+                })
+                if err != nil {
+                    log.Fatal(err)
+                }
+                claims := token.Claims.(jwt.MapClaims)
+                //解釈したPAYLOAD:DATA ["name"]の確認
+
+                // fmt.Println(claims["name"].(string))
+                // claims_id := claims["id"]
+                // fmt.Println(claims_id)
+
+
+                var user NewUser
+                user.ID = int(claims["id"].(float64))
+                user.Name = claims["name"].(string)
+                // json_res, _ := json.Marshal(user)
+                // fmt.Println(string(json_res))
+                // fmt.Println(user.ID)
+
+                str_id := strconv.Itoa(user.ID)
+                // fmt.Println(str_id)
+
+
+                    //mysqlへ接続。ドライバ名（mysql）と、ユーザー名・データソース(ここではgosample)を指定。
+                db, err := sql.Open("mysql", "root@/ca_tech_dojo")
+                log.Println("Connected to mysql.")
+
+                //接続でエラーが発生した場合の処理
+                if err != nil {
+                    log.Fatal(err)
+                }
+                defer db.Close()
+
+                //データベースへクエリを送信。引っ張ってきたデータがrowsに入る。
+                // rows, err := db.Query("INSERT USER VALUES (1, " + string(hello.Name) + ",'Kyoto');")
+                rows, err := db.Query("SELECT name FROM user WHERE id = " + str_id + ";")
+                // rows, err := db.Query(fmt.Sprintf("INSERT USER VALUES (1, " + "Satou" + ", 'Kyoto');"))
+                defer rows.Close()
+                if err != nil {
+                    panic(err.Error())
+                }
+
+                for rows.Next() {
+                    
+                    var res_data InputJsonSchema
+                    err := rows.Scan(&res_data.Name)
+                    res_json, _ := json.Marshal(res_data)
+            
+                    if err != nil {
+                        panic(err.Error())
+                    }
+                    // fmt.Println(person.ID, person.Name)
+                    fmt.Println(string(res_json))
+                    w.Header().Set("Content-Type", "application/json")
+                    w.Write(res_json)
+                }
+
+
+
+
+            default:
+                // w.WriteHeader(http.StatusMethodNotAllowed)
+                fmt.Fprint(w, "Method not allowed.\n")
+
+                // ody := r.Body
+
+        }
+    })
+
+
+
+
+
+
+    
+    
+    http.HandleFunc("/user/update", func(w http.ResponseWriter, r *http.Request) {
+        // request bodyの読み取り
+    
+                //test
             fmt.Fprintf(w, "<h1>Test</h1>")
     
             switch r.Method {
@@ -179,7 +289,7 @@ func main() {
                     // fmt.Println(str_id)
 
 
-                     //mysqlへ接続。ドライバ名（mysql）と、ユーザー名・データソース(ここではgosample)を指定。
+                        //mysqlへ接続。ドライバ名（mysql）と、ユーザー名・データソース(ここではgosample)を指定。
                     db, err := sql.Open("mysql", "root@/ca_tech_dojo")
                     log.Println("Connected to mysql.")
 
@@ -191,7 +301,7 @@ func main() {
 
                     //データベースへクエリを送信。引っ張ってきたデータがrowsに入る。
                     // rows, err := db.Query("INSERT USER VALUES (1, " + string(hello.Name) + ",'Kyoto');")
-                    rows, err := db.Query("SELECT id,name FROM user WHERE id = " + str_id + ";")
+                    rows, err := db.Query("SELECT name FROM user WHERE id = " + str_id + ";")
                     // rows, err := db.Query(fmt.Sprintf("INSERT USER VALUES (1, " + "Satou" + ", 'Kyoto');"))
                     defer rows.Close()
                     if err != nil {
@@ -199,15 +309,18 @@ func main() {
                     }
 
                     for rows.Next() {
-                    
-                        err := rows.Scan(&user.ID, &user.Name)
-                        res_json, _ := json.Marshal(user)
+                        
+                        var res_data InputJsonSchema
+                        err := rows.Scan(&res_data.Name)
+                        res_json, _ := json.Marshal(res_data)
                 
                         if err != nil {
                             panic(err.Error())
                         }
                         // fmt.Println(person.ID, person.Name)
                         fmt.Println(string(res_json))
+                        w.Header().Set("Content-Type", "application/json")
+                        w.Write(res_json)
                     }
 
 
