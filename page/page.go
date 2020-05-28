c431c20c8e474b909384a6b19f57014d176295f6
squarekzmlviy <square-kzmlviy@outlook.jp>
2020-05-29 00:16:28 +0900

    Delete unnecessary processing

--- page/page.go
+++ page/page.go
@@ -47,17 +47,14 @@ func Create(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Print(err)
 		}
-		defer db.Close()
 
-		// rows, err := db.Query("INSERT INTO USER(name,token) VALUES ('" + string(UserData.Name) + "','init');")
 		_, err = db.Exec("INSERT INTO USER(name,token) VALUES ('" + string(UserData.Name) + "','init');")
 		if err != nil {
 			log.Print(err)
 		}		
-		// defer rows.Close()
 
 		rows_out_user, err_out_user := db.Query("SELECT id,name FROM user ORDER BY id DESC LIMIT 1;")
-		defer rows_out_user.Close()
+		// defer rows_out_user.Close()
 		if err_out_user != nil {
 			log.Print(err_out_user)
 		}
